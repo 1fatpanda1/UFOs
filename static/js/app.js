219b767c20bfd9cb0149e9e886d1948eab4b4305
Haynn Tang <thanatoscharun@gmail.com>
2022-09-08 10:13:34 -0600

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -71,9 +71,7 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
 
-    filters.forEach((filterId, elementValue) => {
-      filteredData = filteredData.filter(row => row.filterID === elementValue);
-    });
+   
    
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
